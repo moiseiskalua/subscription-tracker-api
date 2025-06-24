eae5331971e2fb135d1dbecb68c2a1b78d7ff730
Moiseis Kaluã <moiseiskalua@gmail.com>
2025-06-23 21:59:29 -0300

    :sparkles: [Feat]: Create the project and some routes

---
+++ routes/subscription.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', (req, res) => res.send('GET all subscriptions'));
+subscriptionRouter.get('/:id', (req, res) => res.send('GET subscriptions details'));
+subscriptionRouter.post('/', (req, res) => res.send('CREATE subscription'));
+subscriptionRouter.put('/:id', (req, res) => res.send('UPDATE SUBSCRIPTION'));
+subscriptionRouter.delete('/:id', (req, res) => res.send('DELETE subscription'));
+subscriptionRouter.get('/user/:id', (req, res) => res.send('GET all user subscriptions'));
+subscriptionRouter.put('/:id/cancel', (req, res) => res.send('CANCEL subscription'));
+subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send('GET upcoming renewals'));
+
+export default subscriptionRouter;
